5c4eb21f174e92ae240c8f89a23570b53c829134
David Caro <me@dcaro.es>
2023-03-03 09:36:44 +0000

    ansible-runner: use it's own directories for the plays
    This allows me to do local development without getting it messed up with
    the autodeploy xd
    
    Signed-off-by: David Caro <me@dcaro.es>

--- roles/ansible-runner/files/ansible-runner.sh
+++ roles/ansible-runner/files/ansible-runner.sh
@@ -5,18 +5,54 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-while true; do
-    cd /home/dcaro/repos/per_user/david-caro/home-lab-secrets
+OPS_DIR=~dcaro/ops
+SECRETS_DIR=$OPS_DIR/home-lab-secrets
+SETUP_DIR=$OPS_DIR/home-lab-setup
+
+
+init() {
+   # bootstrap the setup
+   [[ -d "$OPS_DIR" ]] || mkdir -p "$OPS_DIR"
+   [[ -d "$SECRETS_DIR" ]] || {
+       git clone git@github.com:david-caro/home-lab-secrets.git "$SECRETS_DIR"
+   }
+   [[ -d "$SETUP_DIR" ]] || {
+       git clone git@github.com:david-caro/home-lab-setup.git "$SETUP_DIR"
+   }
+}
+
+
+update_repo() {
+    local repo="${1?No repo passed}"
+    cd "$repo"
     git fetch --all
-    git reset --hard FETCH_HEAD
-    
-    cd /home/dcaro/repos/per_user/david-caro/home-lab-setup
-    git fetch --all
-    git reset --hard FETCH_HEAD
-    
-    ansible-playbook plays/continuous/basic-server-setup.yml
-    ansible-playbook plays/continuous/eopsin.yml
-    ansible-playbook plays/continuous/apps.yml
-    echo "sleeping for 1h"
-    sleep 3600
-done
+    git rebase FETCH_HEAD
+    cd -
+}
+
+
+run_plays() {
+    local plays_dir="${1?No plays_dir passed}"
+    for play in "${plays_dir}"/*.yml; do
+        ansible-playbook "$play"
+    done
+}
+
+
+
+main() {
+    init
+    while true; do
+        update_repo "$SETUP_DIR"
+        update_repo "$SECRETS_DIR"
+
+        run_plays "${SETUP_DIR}/plays/continuous"
+        
+        echo "sleeping for 1h"
+        sleep 3600
+    done
+
+}
+
+
+main "$@"
