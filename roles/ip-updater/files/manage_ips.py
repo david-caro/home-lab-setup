999ea973351af1a437c0f7179596179ea2c1fb61
David Caro <me@dcaro.es>
2023-04-15 22:23:55 +0100

    Add ip-updater service
    Signed-off-by: David Caro <me@dcaro.es>

---
+++ roles/ip-updater/files/manage_ips.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import requests
+from pathlib import Path
+from requests.auth import HTTPBasicAuth
+import logging
+import subprocess
+import click
+
+SUBDOMAINS = [
+    "emby",
+    "eopsin",
+    "expenses",
+    "grafana",
+    "homeassistant",
+    "bigaswedding",
+    "ssh",
+    "fr.ssh",
+    "sudo",
+    "znc",
+    "ceph",
+    "media",
+    "skill-media",
+
+]
+URL_TEMPLATE="http://www.ovh.com/nic/update?system=dyndns&hostname={subdomain}.dcaro.es&myip={ip}"
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+
+@click.group()
+def main():
+    #result = requests.get(URL)
+    pass
+
+@main.command()
+def get_my_ip_v6():
+    result = subprocess.check_output(
+        ["dig", "TXT", "+short", "o-o.myaddr.l.google.com", "@ns1.google.com"]
+    )
+    click.echo(result)
+
+
+@main.command()
+def get_my_ip_v4():
+    result = requests.get("http://ifconfig.me")
+    click.echo(result.text)
+
+
+@click.option("--static-ip", default=None)
+@click.option("--user", required=True)
+@click.option("--password", required=True)
+@main.command()
+def update_ips(static_ip, user, password):
+    if not static_ip:
+        static_path = Path("./static-ip")
+        if static_path.exists():
+            my_ip = static_path.open(encoding="utf-8").read().strip()
+        else:
+            my_ip = requests.get("http://ifconfig.me").text
+    else:
+        my_ip = static_ip
+
+    for subdomain in SUBDOMAINS:
+        result = requests.get(
+            URL_TEMPLATE.format(subdomain=subdomain, ip=my_ip),
+            auth=HTTPBasicAuth(user, password),
+        )
+        if result.status_code != 200:
+            result.raise_for_status()
+        LOGGER.info(f"Refreshed subdomain {subdomain}: {result.text}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
